0c5081100c163fc1c4ad86cfb80a66e793071f9e
Vaibhav Gulati <vaibhav.g@babylion.team>
2025-09-14 01:55:35 +0530

    deployed over gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ModalProvider } from './contexts/ModalContext';
 import Header from './components/Header';
@@ -12,21 +11,22 @@ import './App.css';
 
 function App() {
   return (
-<HashRouter>
-  <AuthProvider>
-    <ModalProvider>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Feed />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-        <AuthModal />
-      </div>
-    </ModalProvider>
-  </AuthProvider>
-</HashRouter>
+    <HashRouter>
+      <AuthProvider>
+        <ModalProvider>
+          <div className="App">
+            <h1>Test - App is working!</h1>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Feed />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+            </Routes>
+            <AuthModal />
+          </div>
+        </ModalProvider>
+      </AuthProvider>
+    </HashRouter>
   );
 }
 
